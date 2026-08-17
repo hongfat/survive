a0bce4e01838769de944d248c69fd6b7cf3318b9
hongfat <jack730224@gmail.com>
2026-08-17 10:01:07 +0800

    feat: add service worker for offline support and implement favicon assets

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'survive-v1';
+const CACHE_NAME = 'survive-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.webmanifest',
+  './favicon.ico',
   './favicon.svg',
   './icon.svg'
 ];
